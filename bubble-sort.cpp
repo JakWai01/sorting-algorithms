4769e4e355d7815831f578459c5d30dcfbc8fb06
Jakob Waibel <59575534+JakWai01@users.noreply.github.com>
2020-09-08 21:57:59 +0200

    Update bubble-sort.cpp

--- bubble-sort.cpp
+++ bubble-sort.cpp
@@ -1,7 +1,5 @@
-// Array to be sorted
 int array[] = {9,8,7,3,6,1,2};
 
-// Bubble sort O(n^2)
 for (int i = 0; i < sizeof(array)/sizeof(array[0]); i++) {
     for (int j = 0; j < sizeof(array)/sizeof(array[0])-1; j++) {
         if (array[j] > array[j+1]) {
@@ -10,7 +8,6 @@ for (int i = 0; i < sizeof(array)/sizeof(array[0]); i++) {
     }
 }
 
-// Printing out the Array
 for (int i : array){
     cout << i;
 }
